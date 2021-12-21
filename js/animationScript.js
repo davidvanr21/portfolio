4608a9f9a23dcbf76b54bcf3645c202809b89eb5
davidvanr21 <50579850+davidvanr21@users.noreply.github.com>
2021-12-21 16:43:55 +0100

    Animation json import test

---
+++ js/animationScript.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById('headerAnimation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'animation_david.json'
+})
