a8ecb128b8a26459d95e2cb73fa9641d5fae2af6
davidvanr21 <50579850+davidvanr21@users.noreply.github.com>
2024-02-24 16:20:16 +0100

    Homepage improvements responsive design & mail copy

--- js/script.js
+++ js/script.js
@@ -16,50 +16,107 @@ document.getElementById('jaar').innerHTML = jaar;
 
 
 
+// Copy email to clipboard
+// document.getElementById("emailText").addEventListener("click", function() {
+//     // Email address to copy to clipboard
+//     var email = "davidvanrumpt@gmail.com";
+    
+//     // Create a temporary textarea element to hold the email address
+//     var tempInput = document.createElement("textarea");
+//     tempInput.value = email;
+//     document.body.appendChild(tempInput);
+    
+//     // Select and copy the email address
+//     tempInput.select();
+//     document.execCommand("copy");
+    
+//     // Remove the temporary textarea
+//     document.body.removeChild(tempInput);
+    
+//     // Alert to notify the user
+//     alert("Email copied to clipboard: " + email);
+// });
 
 
 
 
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    var emailText = document.getElementById("emailText");
+    var mailCopy = document.getElementById("mail_copy");
+    var mailCopied = document.getElementById("mail_copied");
 
-// Currently working on element
-var tickerWrapper = document.querySelector(".tickerwrapper");
-var list = tickerWrapper.querySelector("ul.list");
-var clonedList = list.cloneNode(true);
-var listItems = list.querySelectorAll("li");
-var listWidth = 10;
+    // Toon mail_copy na hover op emailText
+    emailText.addEventListener("mouseover", function() {
+        mailCopy.style.visibility = "visible";
+    });
 
-// Calculating total width of list
-listItems.forEach(function(item) {
-    listWidth += item.offsetWidth;
+    // Verberg mail_copy na het verlaten van emailText
+    emailText.addEventListener("mouseout", function() {
+        mailCopy.style.visibility = "hidden";
+    });
+
+    // Kopieer e-mailadres naar klembord en toon mail_copied na klik op emailText
+    emailText.addEventListener("click", function() {
+        var email = emailText.textContent.trim();
+        navigator.clipboard.writeText(email).then(function() {
+            mailCopy.style.visibility = "hidden";
+            mailCopy.style.display = "none";
+
+            mailCopied.style.visibility = "visible";
+			mailCopied.style.display = "block";
+            // Verberg mail_copied na 2 seconden
+            setTimeout(function() {
+                mailCopied.style.visibility = "hidden";
+				mailCopied.style.display = "none";
+
+				mailCopy.style.display = "block";
+            }, 2000);
+        }).catch(function(error) {
+            console.error("Failed to copy: ", error);
+        });
+    });
 });
 
-var endPos = tickerWrapper.offsetWidth - listWidth;
 
-// Setting CSS for original and cloned lists
-list.style.width = listWidth + "px";
-clonedList.classList.add("cloned");
-tickerWrapper.appendChild(clonedList);
+// Continious sliding text effect (Currently working on element)
+// var tickerWrapper = document.querySelector(".tickerwrapper");
+// var list = tickerWrapper.querySelector("ul.list");
+// var clonedList = list.cloneNode(true);
+// var listItems = list.querySelectorAll("li");
+// var listWidth = 10;
 
-// TimelineMax animation
-var infinite = new TimelineMax({ repeat: -1, paused: true });
-var time = 40;
+// // Calculating total width of list
+// listItems.forEach(function(item) {
+//     listWidth += item.offsetWidth;
+// });
 
-infinite
-    .fromTo(list, time, { rotation: 0.01, x: 0 }, { force3D: true, x: -listWidth, ease: Linear.easeNone }, 0)
-    .fromTo(clonedList, time, { rotation: 0.01, x: listWidth }, { force3D: true, x: 0, ease: Linear.easeNone }, 0)
-    .set(list, { force3D: true, rotation: 0.01, x: listWidth })
-    .to(clonedList, time, { force3D: true, rotation: 0.01, x: -listWidth, ease: Linear.easeNone }, time)
-    .to(list, time, { force3D: true, rotation: 0.01, x: 0, ease: Linear.easeNone }, time)
-    .progress(1).progress(0)
-    .play();
+// var endPos = tickerWrapper.offsetWidth - listWidth;
 
-// Pause/Play functionality
-tickerWrapper.addEventListener("mouseenter", function() {
-    infinite.pause();
-});
+// // Setting CSS for original and cloned lists
+// list.style.width = listWidth + "px";
+// clonedList.classList.add("cloned");
+// tickerWrapper.appendChild(clonedList);
 
-tickerWrapper.addEventListener("mouseleave", function() {
-    infinite.play();
-});
+// // TimelineMax animation
+// var infinite = new TimelineMax({ repeat: -1, paused: true });
+// var time = 40;
+
+// infinite
+//     .fromTo(list, time, { rotation: 0.01, x: 0 }, { force3D: true, x: -listWidth, ease: Linear.easeNone }, 0)
+//     .fromTo(clonedList, time, { rotation: 0.01, x: listWidth }, { force3D: true, x: 0, ease: Linear.easeNone }, 0)
+//     .set(list, { force3D: true, rotation: 0.01, x: listWidth })
+//     .to(clonedList, time, { force3D: true, rotation: 0.01, x: -listWidth, ease: Linear.easeNone }, time)
+//     .to(list, time, { force3D: true, rotation: 0.01, x: 0, ease: Linear.easeNone }, time)
+//     .progress(1).progress(0)
+//     .play();
+
+// // Pause/Play functionality
+// tickerWrapper.addEventListener("mouseenter", function() {
+//     infinite.pause();
+// });
+
+// tickerWrapper.addEventListener("mouseleave", function() {
+//     infinite.play();
+// });
--- readme.md
+++ readme.md
@@ -1,20 +1,22 @@
-# Portfolio 2021 - David van Rumpt
+# Portfolio 2024 - David van Rumpt
 ![](https://github.com/davidvanr21/portfolio/blob/main/img/github/animatedName_git.gif) 
 
-Watch my best projects on my portfolio.
+A creative environment to show some of my best work to the audience.
 
-
-## In progress
-This project will never be done. There will always be new ideas and more recent projects that I'm working on. Feel free to contact me so we can talk about some work.
 
 ## Credits
-- Big thanks to [Ralf Zonneveld](https://github.com/ralfz123) for helping me with install and put this site online.
-- Also a big thanks to [Inju Michorius](https://github.com/InjuMichorius)!
+- Big thanks to [Ralf Zonneveld](https://github.com/ralfz123) for helping out with install and put this site online.
+- Also a big thanks to [Inju Michorius](https://github.com/InjuMichorius) who always looks at my projects with a keen eye. 
 - FLARE template by [styleshout](https://www.styleshout.com/).
 
 
 ## License & Copyright
-Licensed under the [MIT License](https://github.com/davidvanr21/functional-programming/blob/main/LICENSE) by © [David van Rumpt](https://github.com/davidvanr21), 2020
+Licensed under the [MIT License](https://github.com/davidvanr21/functional-programming/blob/main/LICENSE) by © [David van Rumpt](https://github.com/davidvanr21), 2024
+
+
+Copyright Flare 2024,
+
+I used the [StyleShout](https://styleshout.com/) template as reference. This template has helped me enormously to create a good foundation. After this I started applying my own creativity and code to make it more my own.
 
 FLARE is a free landing page and website template designed for digital agency or startup. With 
 this template, you can now easily create a website that would make a strong and lasting impression. 
